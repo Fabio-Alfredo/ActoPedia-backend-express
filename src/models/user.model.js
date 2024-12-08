c7b596b0032e244f9d1031c3d515269f429de5c6
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-08 12:13:36 -0600

    add: model user whit relation :bricks:

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        },
+        reviews:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Review'
+            }
+        ]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = model('User', userSchema);
+
+export default User;
