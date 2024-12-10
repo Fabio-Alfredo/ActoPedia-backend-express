24f6da6efe9fc7dd0a8cdbead66897a9475a6515
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-09 19:29:42 -0600

    add: error code services users :sparkles:

---
+++ src/utils/errorCodes.util.js
@@ -0,0 +1,15 @@
+const errorCodes = {
+    USER:{
+        NOT_FOUND: 1000,
+        ALREADY_EXISTS: 1001,
+        INVALID_EMAIL: 1002,
+        INVALID_PASSWORD:   1003,
+        INVALID_NAME: 1004,
+        INVALID_ID: 1005,
+        INVALID_ROLE: 1006,
+        INVALID_STATUS: 1007,
+        
+    }
+}
+
+export default errorCodes;
