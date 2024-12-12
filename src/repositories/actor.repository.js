ae0c7a416511d694681b3a3ddd05f0efd7f00290
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-12 11:23:13 -0600

    add: create and update actor in db

---
+++ src/repositories/actor.repository.js
@@ -0,0 +1,18 @@
+import Actor from "../models/actor.model";
+
+export const createActor = async (actor)=>{
+    const newActor = new Actor(actor);
+    return newActor.save();
+}
+
+export const updateActor = async(actorId, actor)=>{
+    return Actor.findByIdAndUpdate({_id: actorId}, actor, {new: true});
+}
+
+export const getActors = async ()=>{
+    return Actor.find().populate('movies');
+}
+
+export const addMovie = async (actorId, movie)=>{
+    return Actor.findByIdAndUpdate({_id: actorId}, {$addToSet: {movies: movie}});
+}
