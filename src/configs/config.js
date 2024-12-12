3ba5e6cf4440078edaf532e3fe497b148c4a772e
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-12 11:08:12 -0600

    add: cloudinary for save images and new dependency for utils :heavy_plus_sign:

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,17 +1,31 @@
-import 'dotenv/config.js'
+import "dotenv/config.js";
 
-const validatorEnv = (env, name)=>{
-    if(!env){
-        throw new Error(`${name} is required`)
-    }
-    return env
-}
+const validatorEnv = (env, name) => {
+  if (!env) {
+    throw new Error(`${name} is required`);
+  }
+  return env;
+};
 
 export const config = {
-    port: validatorEnv(process.env.PORT, 'PORT'),
-    mongo: validatorEnv(process.env.MONGODB_URI, 'MONGODB_URI'),
-    roles: validatorEnv(process.env.ROLES, 'ROLES').split(',').map(role=>role.trim()),
-    defaultRole: validatorEnv(process.env.DEFAULT_ROLE, 'DEFAULT_ROLE').trim(),
-    salt: validatorEnv(process.env.SALT_ROUNDS, 'SALT_ROUNDS'),
-    jwtSecret: validatorEnv(process.env.JWT_SECRET, 'JWT_SECRET'),
-}
+  port: validatorEnv(process.env.PORT, "PORT"),
+  mongo: validatorEnv(process.env.MONGODB_URI, "MONGODB_URI"),
+  roles: validatorEnv(process.env.ROLES, "ROLES")
+    .split(",")
+    .map((role) => role.trim()),
+  defaultRole: validatorEnv(process.env.DEFAULT_ROLE, "DEFAULT_ROLE").trim(),
+  salt: validatorEnv(process.env.SALT_ROUNDS, "SALT_ROUNDS"),
+  jwtSecret: validatorEnv(process.env.JWT_SECRET, "JWT_SECRET"),
+  cloudinaryName: validatorEnv(
+    process.env.CLOUDINARY_CLOUD_NAME,
+    "CLOUDINARY_NAME"
+  ),
+  cloudinaryApiKey: validatorEnv(
+    process.env.CLOUDINARY_API_KEY,
+    "CLOUDINARY_API_KEY"
+  ),
+  cloudinaryApiSecret: validatorEnv(
+    process.env.CLOUDINARY_API_SECRET,
+    "CLOUDINARY_API_SECRET"
+  ),
+};
