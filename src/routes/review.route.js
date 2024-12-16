47b3b2b1f559a8a96415c2d7129b29f30d7b4678
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-16 14:12:20 -0600

    add: usage validator for revies :sparkles:

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,9 +1,22 @@
-import { Router } from 'express';
-import *as reviewController from '../controllers/review.controller.js';
+import { Router } from "express";
+import * as reviewController from "../controllers/review.controller.js";
+import * as reviewValidator from "../validators/review.validator.js";
+import runValidation from "../middlewares/validator.middleware.js";
 
 const reviewRouter = Router();
 
-reviewRouter.post('/create', reviewController.createReview);
-reviewRouter.put('/update/:reviewId', reviewController.updateReview);
+reviewRouter.post(
+  "/create",
+  reviewValidator.createReview,
+  runValidation,
+  reviewController.createReview
+);
+
+reviewRouter.put(
+  "/update/:reviewId",
+  reviewValidator.updateReview,
+  runValidation,
+  reviewController.updateReview
+);
 
 export default reviewRouter;
