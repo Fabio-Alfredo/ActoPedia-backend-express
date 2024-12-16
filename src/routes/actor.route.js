666dd37f3f5752b77d369f6680031aeef11b9eea
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-16 12:06:03 -0600

    add: validator for create and update actors :sparkles:

--- src/routes/actor.route.js
+++ src/routes/actor.route.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import *as actorController from "../controllers/actor.controller.js";
+import * as actorController from "../controllers/actor.controller.js";
+import * as actorValidator from "../validators/actor.validator.js";
 import runValidation from "../middlewares/validator.middleware.js";
 
-
 const actorRouter = Router();
-actorRouter.post("/create", actorController.createActor);
+actorRouter.post(
+  "/create",
+  actorValidator.createActorValidator,
+  runValidation,
+  actorController.createActor
+);
+
+actorRouter.put(
+  "/:actorId",
+  actorValidator.updateActorValidator,
+  runValidation,
+  actorController.updateActor
+);
+
 actorRouter.get("/", actorController.getActors);
-actorRouter.put("/:actorId", actorController.updateActor);
 
 export default actorRouter;
---
+++ src/validators/actor.validator.js
@@ -0,0 +1,37 @@
+import { body } from "express-validator";
+
+export const createActorValidator = [
+  body("name")
+    .trim()
+    .isString()
+    .notEmpty()
+    .withMessage("Name is required")
+    .bail(),
+  body("age").isNumeric().notEmpty().withMessage("Age is required").bail(),
+  body("biography")
+    .isString()
+    .notEmpty()
+    .withMessage("Biography is required")
+    .bail(),
+];
+
+export const updateActorValidator = [
+  body("name")
+    .optional()
+    .isString()
+    .notEmpty()
+    .withMessage("Name is required")
+    .bail(),
+  body("age")
+    .optional()
+    .isNumeric()
+    .notEmpty()
+    .withMessage("Age is required")
+    .bail(),
+  body("biography")
+    .optional()
+    .isString()
+    .notEmpty()
+    .withMessage("Biography is required")
+    .bail(),
+];
