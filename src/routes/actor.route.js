f4e5e87b7e75f56e3835aeeb09697da0e6c5288c
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-16 14:58:06 -0600

    refactor: validation authenticacion user for usage routes :lock:

--- src/routes/actor.route.js
+++ src/routes/actor.route.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import * as actorController from "../controllers/actor.controller.js";
 import * as actorValidator from "../validators/actor.validator.js";
+import * as authMiddleware from "../middlewares/auth.middleware.js";
 import runValidation from "../middlewares/validator.middleware.js";
+import { config } from "../configs/config.js";
 
 const actorRouter = Router();
 actorRouter.post(
   "/create",
+  authMiddleware.authMiddleware,
+  authMiddleware.rolesMiddleware([config.role_one, config.role_three]),
   actorValidator.createActorValidator,
   runValidation,
   actorController.createActor
@@ -13,6 +17,8 @@ actorRouter.post(
 
 actorRouter.put(
   "/:actorId",
+  authMiddleware.authMiddleware,
+  authMiddleware.rolesMiddleware([config.role_one, config.role_three]),
   actorValidator.updateActorValidator,
   runValidation,
   actorController.updateActor
--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import * as reviewController from "../controllers/review.controller.js";
 import * as reviewValidator from "../validators/review.validator.js";
+import * as authMiddleware from "../middlewares/auth.middleware.js";
 import runValidation from "../middlewares/validator.middleware.js";
 
 const reviewRouter = Router();
 
 reviewRouter.post(
   "/create",
+  authMiddleware.authMiddleware,
   reviewValidator.createReview,
   runValidation,
   reviewController.createReview
@@ -14,6 +16,7 @@ reviewRouter.post(
 
 reviewRouter.put(
   "/update/:reviewId",
+  authMiddleware.authMiddleware,
   reviewValidator.updateReview,
   runValidation,
   reviewController.updateReview
