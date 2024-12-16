fb93a05439d98a1be8272da29d5a73adc2f9ce54
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-16 14:09:24 -0600

    add: validator for create and update reviews :sparkles

---
+++ src/validators/review.validator.js
@@ -0,0 +1,41 @@
+import { body, param } from "express-validator";
+
+export const createReview = [
+    body("content")
+        .exists()
+        .withMessage("content is required")
+        .isString()
+        .withMessage("content must be a string"),
+    body("qualification")
+        .exists()
+        .withMessage("qualification is required")
+        .isNumeric()
+        .withMessage("qualification must be a number"),
+    body("user")
+        .exists()
+        .withMessage("user is required")
+        .isMongoId()
+        .withMessage("user must be a valid mongo id"),
+    body("movie")
+        .exists()
+        .withMessage("movie is required")
+        .isMongoId()
+        .withMessage("movie must be a valid mongo id"),
+];
+
+export const updateReview = [
+    param("reviewId")
+        .exists()
+        .withMessage("reviewId is required")
+        .isMongoId()
+        .withMessage("reviewId must be a valid mongo id"),
+    body("content")
+        .optional()
+        .isString()
+        .withMessage("content must be a string"),
+    body("qualification")
+        .optional()
+        .isNumeric()
+        .withMessage("qualification must be a number"),
+
+]
